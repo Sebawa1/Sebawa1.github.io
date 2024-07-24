4c0da14c6a62ed4865b224c3a90546a48c753088
Seba <sebadgb2015@gmail.com>
2024-07-23 23:46:10 -0400

    Tarea Base

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
